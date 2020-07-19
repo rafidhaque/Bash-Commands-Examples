d272d15016873a815dfcbbbd7dc6d66559dcff67
rafidhaque <rafhaq4@gmail.com>
2020-07-19 16:25:23 +0600

    Create crdir.sh

---
+++ crdir.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Author : Rafid Haque
+# Institution : IUT
+
+Help()
+{
+   # Display Help
+   echo "-./crdir.sh creates files in the Documents directory."
+   echo "- Syntax : ./crdir.sh <dir> <subdir_1> <subdir_2> ... <subdir_N>"
+   echo "-- Parameter 1 <dir> : Name of the parent directory."
+   echo "-- Parameter 2 <subdir_1> : Name of the 1st sub-directory."
+   echo "--."
+   echo "-- Parameter N <subdir_N> : Name of the Nth sub-directory."
+}
+
+
+arguments=$#
+arguments=$(( arguments - 1))
+
+i=0
+list_arguments=( "$@" )
+
+if [[ $arguments == -1 ]]
+then
+  echo "Syntax Error!!"
+  echo "Syntax : ./crdir.sh <dir> <subdir_1> <subdir_2> ... <subdir_N>"
+  echo "For more information type: ./crdir.sh -h"
+elif [[ "$list_arguments[0]" -eq "-h" ]]
+then
+  Help
+else
+  until [ $i -gt $arguments ]
+  do
+    mkdir ${list_arguments[i]}
+    cd ${list_arguments[i]}
+    i=$(( i +1 ))
+  done
+fi
