20af279aa5ba30b4f57907ce40ce0b2ec911eb13
rafidhaque <rafhaq4@gmail.com>
2020-07-19 18:03:41 +0600

    Update factorial.sh
    Done Factorial

--- factorial.sh
+++ factorial.sh
@@ -2,3 +2,17 @@
 
 # Author : Rafid Haque
 # Institution : IUT
+
+echo "Enter a number: "
+read number
+
+temp=1
+i=1
+
+until [ $i -gt $number ]
+do
+  temp=$(( temp * i ))
+  i=$(( i +1 ))
+done
+
+echo Factorial of $number is: $temp
